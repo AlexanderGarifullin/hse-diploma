51f955cf6cac69100d27cf8e3f9633accafcd867
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-04-21 14:56:23 +0500

    feat: added the User entity

---
+++ web-server/src/main/java/hse/dss/entity/User.java
@@ -0,0 +1,26 @@
+package hse.dss.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", schema = "diplom")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
