6891b0435f2275e8edb6c46c6889361e42e7cc92
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-04-24 21:55:41 +0500

    feat: added a usage example

--- parser-lib/src/main/java/hse/diploma/ParserMain.java
+++ parser-lib/src/main/java/hse/diploma/ParserMain.java
@@ -1,6 +1,21 @@
 package hse.diploma;
 
+import hse.diploma.export.Export;
+import hse.diploma.parser.SchemaRegexBuilder;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class ParserMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String latex = """
+            Первая строка содержит целое число t (1≤t≤10^4) — количество тестов.
+            Первая строка каждого теста содержит целое число n (1≤n≤2⋅10^5).
+            Вторая строка каждого теста содержит n целых чисел a1,a2,…,a_n (1≤a_i≤n).
+            Гарантируется, что сумма n по всем тестам не превосходит 2⋅10^5.
+            """;
+        var schema = SchemaRegexBuilder.parse(latex);
+        System.out.println(Export.asPrettyText(schema));
+        Export.saveToJson(schema, Path.of("parser-lib-samples/schema.json"));
     }
 }
